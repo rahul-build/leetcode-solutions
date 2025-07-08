c7cb21d4ef8bb08f8bd11b845a5be31e02fac4ba
rahul-build <rahulbuild.dev@gmail.com>
2025-07-08 14:02:26 +0530

    insert at the end of linkedlist  using recursion

---
+++ at_end_recursion.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+using namespace std;
+
+class Node
+{
+public:
+    int data;
+    Node *next;
+
+    Node(int value)
+    {
+        data = value;
+        next = NULL;
+    }
+};
+
+Node *createLinkedList(int arr[], int index, int size)
+{
+    if (index == size)
+    {
+        return NULL;
+    }
+    Node *temp = new Node(arr[index]);
+    temp->next = createLinkedList(arr, index + 1, size);
+    return temp;
+}
+
+void printList(Node *head)
+{
+    while (head != NULL)
+    {
+        cout << head->data << " ";
+        head = head->next;
+    }
+    cout << endl;
+}
+
+int main()
+{
+    int arr[] = {1, 2, 3, 4, 5};
+    Node *Head = createLinkedList(arr, 0, 5);
+    printList(Head);
+    return 0;
+}
