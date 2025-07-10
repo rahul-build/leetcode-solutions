49baaac0fc91616f9a34f20435bc43b60354ddbe
rahul-build <rahulbuild.dev@gmail.com>
2025-07-10 14:39:52 +0530

    feat:code for delete head nodde from linkedlist

--- Linked_List/delete_first.cpp
+++ Linked_List/delete_first.cpp
@@ -1,3 +1,4 @@
+
 #include <iostream>
 using namespace std;
 
@@ -6,6 +7,7 @@ class Node
 public:
     int data;
     Node *next;
+
     Node(int value)
     {
         data = value;
@@ -13,24 +15,19 @@ public:
     }
 };
 
+// Recursive function to create linked list
 Node *createLinkedList(int arr[], int index, int size)
 {
     if (index == size)
+    {
         return NULL;
+    }
     Node *temp = new Node(arr[index]);
     temp->next = createLinkedList(arr, index + 1, size);
     return temp;
 }
 
-Node *deleteFirstNode(Node *head)
-{
-    if (head == NULL)
-        return NULL;
-    Node *temp = head->next;
-    delete head;
-    return temp;
-}
-
+// Function to print the list
 void printList(Node *head)
 {
     while (head != NULL)
@@ -44,10 +41,7 @@ void printList(Node *head)
 int main()
 {
     int arr[] = {1, 2, 3, 4, 5};
-
-    Node *head = createLinkedList(arr, 0, 5); // step 1
-    head = deleteFirstNode(head);             // step 2
-    printList(head);                          // step 3
-
+    Node *Head = createLinkedList(arr, 0, 5);
+    printList(Head);
     return 0;
 }
